1cd6e45f3f222051d62dac18d5dd11bfab544064
Brandon James Shaffer <bjs397@nau.edu>
2024-02-27 19:57:35 -0700

    Added a few more components such as LandingPage, Footer, and AboutMe

--- bjshaf-app/src/App.js
+++ bjshaf-app/src/App.js
@@ -2,12 +2,14 @@ import './App.css'
 import NavigationBar from './components/NavigationBar'
 import Footer from './components/Footer'
 import LandingPage from './components/LandingPage'
+import AboutMe from './components/AboutMe'
 
 function App() {
     return (
         <div className="App">
             <NavigationBar />
             <LandingPage />
+            <AboutMe />
             <Footer />
         </div>
     )
