483fa2ad0b7841252797a6c39884210eb16b37d0
Brandon <bjs397@nau.edu>
2026-05-23 07:59:55 -0700

    Changed disclaimer text color to white for better contrast.

--- bjshaf-app/src/components/Project1.js
+++ bjshaf-app/src/components/Project1.js
@@ -17,7 +17,7 @@ class Project1 extends Component {
                             style={{ display: 'block', margin: 'auto' }}
                         />
                     </a>
-                    <p style={{ fontSize: '0.75rem', color: '#888', textAlign: 'center', marginTop: '6px' }}>
+                    <p style={{ fontSize: '0.75rem', color: '#ffffff', textAlign: 'center', marginTop: '6px' }}>
                         Note: The hosting service may take up to 30 seconds to serve data after 15 minutes of inactivity.
                     </p>
                 </div>
--- bjshaf-app/src/components/Project2.js
+++ bjshaf-app/src/components/Project2.js
@@ -54,7 +54,7 @@ class Project2 extends Component {
                             height="120%"
                         />
                     </a>
-                    <p style={{ fontSize: '0.75rem', color: '#888', textAlign: 'center', marginTop: '6px' }}>
+                    <p style={{ fontSize: '0.75rem', color: '#ffffff', textAlign: 'center', marginTop: '6px' }}>
                         Note: The hosting service may take up to 30 seconds to serve data after 15 minutes of inactivity.
                     </p>
                 </div>
